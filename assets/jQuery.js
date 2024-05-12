ae38589cbacd2f9b931b33350b56a7498c6d1472
{M.Komiya} <{mayu.komiya54@gmail.com}>
2024-05-12 22:23:35 +0900

    test

--- assets/jQuery.js
+++ assets/jQuery.js
@@ -17,36 +17,6 @@ $(function () {
   });
 });
 
-function FixedAnime() {
-  var elemTop = $("#ham-area").offset().top; //#ham-areaの位置まできたら
-  var scroll = $(window).scrollTop();
-  if (scroll <= 20) {
-    //上から20pxスクロールされたら
-    $("#btn").addClass("DownMove"); //DownMoveというクラス名を除き
-  } else if (scroll >= elemTop) {
-    $("#btn").removeClass("UpMove"); //#headerについているUpMoveというクラス名を除く
-    $("#btn").addClass("DownMove"); //#headerについているDownMoveというクラス名を付与
-  } else {
-    if ($("#btn").hasClass("DownMove")) {
-      //すでに#headerにDownMoveというクラス名がついていたら
-      $("#btn").removeClass("DownMove"); //DownMoveというクラス名を除き
-      $("#btn").addClass("UpMove"); //UpnMoveというクラス名を付与
-    }
-  }
-}
-
-// //ナビゲーションをクリックした際のスムーススクロール
-// $('#btn a').click(function () {
-//   var elmHash = $(this).attr('href'); //hrefの内容を取得
-//   var pos = Math.round($(elmHash).offset().top-70); //headerの高さを引く
-//   $('body,html').animate({scrollTop: pos}, 500);//取得した位置にスクロール※数値が大きいほどゆっくりスクロール
-//   return false;//リンクの無効化
-// });
-
-// 画面をスクロールをしたら動かしたい場合の記述
-$(window).scroll(function () {
-  FixedAnime(); /* スクロール途中からヘッダーを出現させる関数を呼ぶ*/
-});
 
 //page-top//
 $(function () {
