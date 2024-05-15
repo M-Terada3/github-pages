7d1adbcdb6aee62f733244daf01260be9e70c6e0
{M.Komiya} <{mayu.komiya54@gmail.com}>
2024-05-15 22:45:31 +0900

    ポートフォリオ①作業

--- assets/jQuery.js
+++ assets/jQuery.js
@@ -1,13 +1,30 @@
-const images = document.querySelectorAll("p-pickUp__contents img");
-let number = 0;
+//sp fixMenu//
+var navPos = jQuery( '#mainMenu' ).offset().top;
+var navHeight = jQuery( '#mainMenu' ).outerHeight();
+jQuery( window ).on( 'scroll', function() {
+	if ( jQuery( this ).scrollTop() > navPos ) {
+		jQuery( 'main' ).css( 'padding-top', navHeight );
+		jQuery( '#mainMenu' ).addClass( 'm_fixed' );
+	} else {
+		jQuery( 'main' ).css( 'padding-top', 0 );
+		jQuery( '#mainMenu' ).removeClass( 'm_fixed' );
+	}
+});
 
-function changeImage() {
-  images[number].classList.remove("active");
-  number = (number + 1) % images.length;
-  images[number].classList.add("active");
-}
+//pc fixMenu//
+$(function () {
+	$(window).scroll(function () {
+		var scroll = $(window).scrollTop();
 
-setInterval(changeImage, 2000);
+		if (scroll > 700) {
+			$('#fixNav').addClass('is-show');
+		}
+		else {
+			$('#fixNav').removeClass('is-show');
+		}
+	});
+});
+
 
 //hamburger nav//
 $(function () {
@@ -15,13 +32,21 @@ $(function () {
     $(".btn").toggleClass("close");
     $("nav").fadeToggle(500);
   });
-  // $(window).scroll(function () {
-  //   if ($(window).scrollTop() > 1) {
-  //     $("btn").fadeIn(300).css("display", "flex");
-  //   } else {
-  //     $("btn").fadeOut(300);
-  //   }
-  // });
+});
+
+$(function () {
+  $(".fixBtn").on("click", function () {
+    $(".fixBtn").toggleClass("close");
+    $("nav").fadeToggle(500);
+  });
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 150 ) {
+      $(".fixBtn").fadeIn();
+    } else {
+      $(".fixBtn").fadeOut(1);
+    }
+  });
 });
 
 //page-top//
